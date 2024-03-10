4291f1ffbfa7278fe5caaf52c742ed625f3c9b3b
Kaushal Bhansali <kaushal.bhansali2@gmail.com>
2024-03-11 04:06:26 +0530

    Updated API endpoint

--- src/configs/ApiConfigs.ts
+++ src/configs/ApiConfigs.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://api.gocareers.in'
+export const API_URL = 'https://4d07kxo8oa.execute-api.ap-south-1.amazonaws.com'
 export enum API_ENDPOINTS {
   GET_JOBS = 'jobs'
 }
